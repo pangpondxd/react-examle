bed6d1b4bc8c4f74f7c0b19044f44d7aa2491e65
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-12-19 09:38:21 +0700

    JSX Retrictions
    1 elements can get in app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+     <p>test JSX</p>
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Work now?'))
+}
+
+export default App;
