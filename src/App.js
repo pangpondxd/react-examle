71e0cccc264500b5e53119f124b46aed7f6f88a0
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-12-23 12:53:33 +0700

    React Hook Switch Name
    React Hook Switch Name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Person from './Person/Person';
-function App() {
+
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {name: 'Tanawat', age:'25'},
+      {name: 'Sirawich', age:'24'},
+      {name: 'Teerapat', age:'10'}
+    ],
+    otherState: 'p'
+  });
+  const [isToggle, setToggle] = useState(false)
+const [keepPerson, setKeepPerson] = useState([])
+
+const switchNameHandle = () => {
+  setKeepPerson(personsState)
+  if(isToggle){
+  setPersonsState({
+    persons: [
+      {name: 'Jo', age:'25'},
+      {name: 'Ex', age:'24'},
+      {name: 'Tee', age:'10'}
+    ],
+    otherState: 'switch1'
+  });
+}else{
+  setPersonsState(keepPerson)
+}
+}
+useEffect(() => {
+  if(isToggle){
+  setToggle(!isToggle)
+  }else{
+    setToggle(!isToggle)
+  }
+}, [keepPerson])
+
   return (
     <div className="App">
       <h1>Hello</h1>
      <p>test JSX</p>
-     <Person />
-     <Person />
-     <Person />
+     <button onClick={() => switchNameHandle()}>Switch Name</button>
+     <div>
+     {personsState.persons.map(m  => (
+<h1>{m.name}</h1>
+     ))}
+    </div>
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Work now?'))
