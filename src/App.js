1649f3a2dc1656989d9580d20aafb7ce0c81e17d
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-12-25 13:42:39 +0700

    Working with inline CSS
    Working with inline CSS

--- src/App.js
+++ src/App.js
@@ -31,11 +31,20 @@ class App extends Component {
   })
   }
 render() {
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
   return (
     <div className="App">
       <h1>Hello</h1>
      <p>test JSX</p>
-     <button onClick={() => this.switchNameHandle('Dukdui')}>Switch Name</button>
+     <button 
+     style={style}
+     onClick={() => this.switchNameHandle('Dukdui')}>Switch Name</button>
      <Person 
      name={this.state.person[0].name} 
      age={this.state.person[0].age} />
