e7678e5933376dbef26d7113f12b5e5343d763d2
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-01-03 13:02:50 +0700

    Rendering Content Conditionally
    Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -1,65 +1,90 @@
-import React, {Component} from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import "./assets/css/tailwind.css";
+import Person from "./Person/Person";
 class App extends Component {
   state = {
     person: [
-      {name: 'Jo', age: 25},
-      {name: 'Ex', age: 24},
-      {name: 'Tee Tee', age: 10}
+      { name: "Jo", age: 25 },
+      { name: "Ex", age: 24 },
+      { name: "Tee Tee", age: 10 }
     ],
-    otherState: 'some other value'
-  }
-  switchNameHandle = (newName) => {
+    otherState: "some other value",
+    showPerson: false
+  };
+  switchNameHandle = newName => {
     // console.log('Clicked!!!')
     //Don't do this this.state.person[0].name = 'Tanawat'
-    this.setState({person: [{
-      name: newName, age: 25},
-      {name: 'Sirawich', age: 24},
-      {name: 'Teerapat', age: 10
-    }
-    ]
-  })
+    this.setState({
+      person: [
+        {
+          name: newName,
+          age: 25
+        },
+        { name: "Sirawich", age: 24 },
+        { name: "Teerapat", age: 10 }
+      ]
+    });
+  };
+  nameChangedHandle = event => {
+    this.setState({
+      person: [
+        {
+          name: "Tanawat",
+          age: 25
+        },
+        { name: event.target.value, age: 24 },
+        { name: "Teerapat", age: 10 }
+      ]
+    });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson
+    this.setState({showPerson: !doesShow})
+
+  };
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+    return (
+      <div className="App">
+        <h1 className="text-red-600"> Hello </h1>{" "}
+        <p className="text-2xl text-red-700"> test JSX </p>{" "}
+        <button style={style} onClick={this.togglePersonHandler}>
+          Switch Name{" "}
+        </button>{" "}
+        {
+        this.state.showPerson === true ? 
+          <div>
+            <Person
+              name={this.state.person[0].name}
+              age={this.state.person[0].age}
+            />{" "}
+            <Person
+              name={this.state.person[1].name}
+              age={this.state.person[1].age}
+              click={() => this.switchNameHandle("Kooooo")}
+              changed={this.nameChangedHandle}
+            >
+              My hobby is badminton{" "}
+            </Person>{" "}
+            <Person
+              name={this.state.person[2].name}
+              age={this.state.person[2].age}
+            ></Person>{" "}
+          </div>
+         : <h1>Hello It's false</h1>
+         }{" "}
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Work now?'))
   }
-  nameChangedHandle = (event) => {
-    this.setState({person: [{
-      name: 'Tanawat', age: 25},
-      {name: event.target.value, age: 24},
-      {name: 'Teerapat', age: 10
-    }
-    ]
-  })
-  }
-render() {
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer'
-  }
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-     <p>test JSX</p>
-     <button 
-     style={style}
-     onClick={() => this.switchNameHandle('Dukdui')}>Switch Name</button>
-     <Person 
-     name={this.state.person[0].name} 
-     age={this.state.person[0].age} />
-     <Person 
-     name={this.state.person[1].name} 
-     age={this.state.person[1].age}
-     click={() => this.switchNameHandle('Kooooo')}
-     changed={this.nameChangedHandle}
-     >My hobby is badminton</Person>
-     <Person 
-     name={this.state.person[2].name} 
-     age={this.state.person[2].age}></Person>
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Work now?'))
-}
 }
 export default App;
